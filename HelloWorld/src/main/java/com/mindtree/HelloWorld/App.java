b6dd35bdf950e160e710c8d52c7b59dbd6b72a88
keerthi-battula <net2bks@gmail.com>
2021-02-15 13:44:24 +0530

    first commit

---
+++ HelloWorld/src/main/java/com/mindtree/HelloWorld/App.java
@@ -0,0 +1,14 @@
+package com.mindtree.HelloWorld;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+        System.out.println("hi everyone");
+    }
+}
